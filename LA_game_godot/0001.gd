f3f24d4ecb621c29f706075519743ea5bd6751cc
KaysData <kay-ayala@protonmail.com>
2023-06-12 19:31:33 -0700

    basic color switching functionality added

--- LA_game_godot/0001.gd
+++ LA_game_godot/0001.gd
@@ -1,15 +1,15 @@
 extends Node3D
 
-
+var change_color = func(color_: Color) -> Color: return color_
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	set_current_color("blue")
 	reassign_colors()
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
+	
 func make_red(color_:Color):
 	color_.h = 0
 	return color_
@@ -21,13 +21,44 @@ func make_green(color_:Color):
 func make_blue(color_:Color):
 	color_.h = 0.585
 	return color_
+	
+func set_current_color(color_name):
+	if color_name == "blue":
+		change_color = make_blue
+	elif color_name == "red":
+		change_color = make_red
+	elif color_name == "green":
+		change_color = make_green
 
 func reassign_colors():
 	var worldEnvironment = get_node("WorldEnvironment")
-	var current_color = worldEnvironment.environment.sky.sky_material.sky_top_color
-
-	var new_color = make_red(current_color)
-	worldEnvironment.environment.sky.sky_material.sky_top_color = new_color
+	var sky_material_ = worldEnvironment.environment.sky.sky_material
+	
+	#Gets current sky box color values 
+	var current_sky_c = sky_material_.sky_top_color
+	var current_sky_horizon_c = sky_material_.sky_horizon_color
+	var current_ground_bottom_c = sky_material_.ground_bottom_color
+	var current_ground_horizon_c = sky_material_.ground_horizon_color
+	
+	#Changes color values
+	var new_sky_c = change_color.call(current_sky_c)
+	var new_sky_horizon_c = change_color.call(current_sky_horizon_c)
+	var new_ground_bottom_c = change_color.call(current_ground_bottom_c)
+	var new_ground_horizon_c = change_color.call(current_ground_horizon_c)
+	
+	#Sets new sky box color values 
+	sky_material_.sky_top_color = new_sky_c
+	sky_material_.sky_horizon_color = new_sky_horizon_c
+	sky_material_.ground_bottom_color = new_ground_bottom_c
+	sky_material_.ground_horizon_color = new_ground_horizon_c
+	
+	#Gets, changes, and sets OmniLight color
+	var local_light = get_node("OmniLight3D")
+	var current_light_c = local_light.light_color
+	var new_light_c = change_color.call(current_light_c)
+	local_light.light_color = new_light_c
+	
+	
 	"
 	var counter = 0
 	for material_ in material_list:
--- LA_game_godot/WorldEnvironment.gd
+++ LA_game_godot/WorldEnvironment.gd
@@ -4,30 +4,10 @@ extends WorldEnvironment
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
-	#reassign_colors()
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-"func make_red(color_:Color):
-	color_.h = 0
-	return color_
-
-func make_green(color_:Color):
-	color_.h = 0.321
-	return color_
-
-func make_blue(color_:Color):
-	color_.h = 0.585
-	return color_
-
-func reassign_colors():
-	var current_color = environment.sky.sky_material.sky_top_color.sky_top_color
-	var new_color = make_red(current_color)
-	environment.sky.sky_material.sky_top_color = new_color
-	
-"
-
 
 
